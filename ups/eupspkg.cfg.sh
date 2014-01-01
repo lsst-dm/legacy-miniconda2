84129b19d0ca49a49e36ee9671122bd8e497b45e
Mario Juric <mjuric@lsst.org>
2014-01-01 04:05:13 -0600

    Anaconda 1.8.0 downloader and installer

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,33 @@
+# EupsPkg config file. Sourced by 'eupspkg'
+
+prep()
+{
+	# Select the apropriate Anaconda distribution
+	OS=$(uname -s -m)
+	case "$OS" in
+		"Linux x86_64")		FN=Anaconda-1.8.0-Linux-x86_64.sh ;;
+		"Linux "*) 		FN=Anaconda-1.8.0-Linux-x86.sh ;;
+		"Darwin x86_64")	FN=Anaconda-1.8.0-MacOSX-x86_64.sh ;;
+		*) 			die "unsupported OS or architecture ($OS). try installing Anaconda manually."
+	esac
+
+	# Prefer system curl; user-installed ones sometimes behave oddly
+	if [[ -x /usr/bin/curl ]]; then
+		CURL=${CURL:-/usr/bin/curl}
+	else
+		CURL=${CURL:-curl}
+	fi
+
+	"$CURL" -s -L -o installer.sh http://repo.continuum.io/archive/$FN
+}
+
+build() { :; }
+
+install()
+{
+	clean_old_install
+
+	bash installer.sh -b -p "$PREFIX"
+
+	install_ups
+}
