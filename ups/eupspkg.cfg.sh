lsst-dm/legacy-miniconda2

commit 810405abcf091def4a6a75e674246b8e76caa00e

pin conda package versions

ups/eupspkg.cfg.sh
@@ -41,16 +41,15 @@ install()
     fi
 
     (
-        # Install packages on which the stack is know to depend
-        # Note: it's not clear if agreement was reached to use all of these,
-        #       or they crept in by accident.
+        # Install packages on which the stack is known to depend
 
-        export PATH="$PREFIX/bin:$PATH"
-        # XXX the list of conda packages should be kept in sync with
-        # https://github.com/lsst/lsstsw/blob/master/bin/deploy
-        # The lsstsw list of packages should be considered authoritative
-        conda install --yes numpy scipy matplotlib requests cython sqlalchemy astropy pandas
-        pip install stsci.distutils
+        export PATH="$LSSTSW/miniconda/bin:$PATH"
+        local tmpdir=${TMPDIR:-/tmp}
+        local reqfile='conda_packages.txt'
+        local baseurl='https://raw.githubusercontent.com/lsst/lsstsw/master/etc/'
+        (cd "$tmpdir"; $CURL -# -L -O "${baseurl}/${reqfile}")
+
+        conda install --yes --file "${tmpdir}/${reqfile}"
     )
 
     install_ups

commit f88f915b06469b3dc478d248b651c023dcbd0eb9

fix PATH construction for conda executation
Broken by 810405a

ups/eupspkg.cfg.sh
@@ -43,7 +43,7 @@ install()
     (
         # Install packages on which the stack is known to depend
 
-        export PATH="$LSSTSW/miniconda/bin:$PATH"
+        export PATH="$PREFIX/bin:$PATH"
         local tmpdir=${TMPDIR:-/tmp}
         local reqfile='conda_packages.txt'
         local baseurl='https://raw.githubusercontent.com/lsst/lsstsw/master/etc/'